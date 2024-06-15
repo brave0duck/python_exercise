e7944408642ed1ed1e8d356d2b1a448bcac5a8e2
brave0duck <newkemp@naver.com>
2024-06-15 23:06:15 +0900

    Update selection_sort2 recu.py

--- selection_sort2 recu.py
+++ selection_sort2 recu.py
@@ -3,10 +3,10 @@ import random
 def selection_sort(list):
         if len(list) <= 1:
             return list
-        min_index = list.index(min(list))
-        list[0], list[min_index] = list[min_index], list[0]
+        min_index = list.index(min(list))        # find min value
+        list[0], list[min_index] = list[min_index], list[0]        # swap value
 
-        return [list[0]] + selection_sort(list[1::])
+        return [list[0]] + selection_sort(list[1::])        # function call other parts
 
 array=[]
 for i in range(10):
